98eb42abd161d2881a1574f7e25e77cb40572893
Mohamed-Omar17 <mooniomar17@gmail.com>
2025-04-24 17:00:42 -0400

    Add project files.

---
+++ TinyFPS/Source.cpp
@@ -0,0 +1,148 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+#include <cmath>
+
+
+
+
+uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255) {
+	return (a << 24) + (b << 16) + (g << 8) + r;
+} //uint# means unsigned integer(can't be negative) and the arguments in the function are used to represent colors
+
+void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &a) {
+	r = (color >> 0) & 255;
+	g = (color >> 8) & 255;
+	b = (color >> 16) & 255;
+	a = (color >> 24) & 255;
+}
+
+
+void drop_ppm_image(const std::string filename, const std::vector<uint32_t>& image, const size_t w, const size_t h) {
+	assert(image.size() == w * h);
+	std::ofstream ofs(filename, std::ios::binary);
+	ofs << "P6\n" << w << " " << h << "\n255\n";
+	for (size_t i = 0; i < h * w; ++i) {
+		uint8_t r, g, b, a;
+		unpack_color(image[i], r, g, b, a);
+		ofs << static_cast<char>(r) << static_cast<char>(g) << static_cast<char>(b);
+	}
+	ofs.close();
+}
+
+
+void draw_rectangle(std::vector<uint32_t>& img, const size_t img_w, const size_t img_h,
+	const size_t x, const size_t y, const size_t w, const size_t h, const uint32_t color) {
+	assert(img.size() == img_w * img_h);
+	for (size_t i = 0; i < w; i++) {
+		for (size_t j = 0; j < h; j++) {
+			size_t cx = x + i;
+			size_t cy = y + j;
+			if (cx >= img_w || cy >= img_h) continue; //no need to check negative values
+			img[cx + cy * img_w] = color;
+		}
+	}
+}
+
+
+int main() {
+	const size_t win_w = 1024; //image width
+	const size_t win_h = 512; //image height
+	std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255,255,255)); //image itself initialized to red
+	
+	const size_t map_w = 16; //map width
+	const size_t map_h = 16; //map height
+	const char map[] = "0000222222220000"\
+						"1              0"\
+						"1      11111   0"\
+						"1     0        0"\
+						"0     0  1110000"\
+						"0     3        0"\
+						"0   10000      0"\
+						"0   0   11100  0"\
+						"0   0   0      0"\
+						"0   0   1  00000"\
+						"0       1      0"\
+						"2       1      0"\
+						"0       0      0"\
+						"0 0000000      0"\
+						"0              0"\
+						"0002222222200000"; // our game map
+	assert(sizeof(map) == map_w * map_h + 1); //+1 for the null terminated string
+	
+	float player_x = 3.456; //player x position
+	float player_y = 2.345; //p-layer y position
+	/*float degrees = 0; //use if you want to use degrees instead of radians
+	double pi = 2 * acos(0.0);
+	float radians = degrees * pi / 180.0;
+	float player_a = radians; //player view direction in radians */
+	float player_a = 1.523;
+	const float fov =  M_PI/ 3.; //field of view is 60 degrees,common for raycasting games like Wolfenstein
+	
+	const size_t ncolors = 10;
+	std::vector<uint32_t> colors(ncolors);
+
+	for (size_t i = 0; i < ncolors; i++) {
+		colors[i] = pack_color(rand() % 255, rand() % 255, rand() % 255);
+	}
+
+
+	const size_t rect_w = win_w / (map_w*2); // 1024/(16*2) = 1024/32 = 32
+	const size_t rect_h = win_h / map_h; // 512/16 = 32
+
+
+	for (size_t frame = 0; frame < 360; frame++)
+	{
+		std::stringstream ss;
+		ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
+		player_a += 2 * M_PI / 360;
+
+		framebuffer = std::vector<uint32_t>(win_w * win_h, pack_color(255, 255, 255)); // clear the screen
+
+
+		for (size_t j = 0; j < map_h; j++) {
+			for (size_t i = 0; i < map_w; i++) {
+				if (map[i + j * map_w] == ' ') continue;
+				size_t rect_x = i * rect_w;
+				size_t rect_y = j * rect_h;
+				size_t icolor = map[i + j * map_w] - '0';
+				assert(icolor < ncolors);
+				draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h,
+					colors[icolor]);
+			}
+		}
+
+		for (size_t i = 0; i < win_w / 2; i++) { //starts image at x = 512 (right side of the screen)
+			float angle = player_a - fov / 2 + fov * i / float(win_w);
+
+			for (float t = 0; t < 20; t += 0.01) { //t is the direction of the array, the number is the distance along the ray where you place the marker
+				float cx = player_x + t * cos(angle); //finds the marker's x position
+				float cy = player_y + t * sin(angle); //fins the marker's y position
+
+				size_t pix_x = cx * rect_w;
+				size_t pix_y = cy * rect_h;
+
+				framebuffer[pix_x + pix_y * win_w] = pack_color(160, 160, 160); //draws visibility cone
+
+
+				if (map[int(cx) + int(cy) * map_w] != ' ') { //draw the vertical column to create an illusion of 3d
+					size_t icolor = map[int(cx) + int(cy) * map_w] - '0';
+					assert(icolor < ncolors);
+					size_t column_height = win_h / (t*cos(angle-player_a));
+					draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2,
+						1, column_height, colors[icolor]);
+					break;
+				}
+			}
+		}
+		drop_ppm_image(ss.str(), framebuffer, win_w, win_h);
+	}
+	
+
+	return 0;
+}
